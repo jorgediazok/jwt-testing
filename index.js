6d4305518b8fd5746865a3bce6ef3d0f8ed710d5
Jorge <jorgediazok@gmail.com>
2020-07-08 01:52:20 -0300

    token added

--- index.js
+++ index.js
@@ -9,6 +9,34 @@ app.get('/', (req, res) => {
   });
 });
 
+app.post('/api/login', (req, res) => {
+  const user = { id: 3 };
+  const token = jwt.sign({ user }, 'my_secret_key');
+  res.json({
+    token,
+  });
+});
+
+app.get('/api/protected', ensuretoken, (req, res) => {
+  res.json({
+    text: 'Protected',
+  });
+});
+
+//Creo un middleware
+
+function ensuretoken(req, res, next) {
+  const bearerHeader = req.headers['authorization'];
+  if (typeof bearerHeader !== 'undefined') {
+    const bearer = bearerHeader.split('');
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+}
+
 app.listen(3000, () => {
   console.log('Server on port 3000');
 });
